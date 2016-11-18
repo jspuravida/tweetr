0ce5f9e0a6dff2c62b872ff15e40a6a1a7a08eec
jspuravida <julian.salari@gmail.com>
2016-11-18 02:13:43 +0000

    Added ajax functionality for fetching tweets in json, and refetch tweets on submission (not refreshing the page upon submission)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,67 +6,27 @@
 
 $(function () {
 
-  var tweetData =[
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+
 ///////////////////CREATING NEW TWEETS
 function renderTweets(tweets) {
+  $listOfTweets = $('.listOfTweets');
+  $listOfTweets.empty();
   tweets.forEach(function (data) {
-    createTweetElement(data);
-    return;
+    $tweet = createTweetElement(data);
+    $listOfTweets.prepend($tweet);
   })
-////////////////////////SAFEGUARDING TEXTFIELDS
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
 }
+
 
 ////// FUNCTION FOR TURNING A TWEET INTO AN ELEMENT ///////
 function createTweetElement(tweet) {
+
+  ////////////////////////SAFEGUARDING TEXTFIELDS
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
 
   var name = tweet.user.name;
   var avatar = tweet.user.avatars.small;
@@ -75,13 +35,8 @@ function createTweetElement(tweet) {
   var date = timeSince(tweet.created_at);
   var footerIcons = '<i class="fa fa-flag" aria-hidden="true"></i><i class="fa fa-retweet" aria-hidden="true"></i><i class="fa fa-heart" aria-hidden="true"></i>';
 
-  // $(".tweeterName").append(name);
-  // $(".avatar").attr('src', avatar)
-  // $(".tweetHandler").append(handle);
-  // $(".tweetMessage").append(content);
-  // $(".footerDate").append(timeSince(date));
 
-  $('.listOfTweets').append(
+  tweet = $(
     `<article class="tweet">
       <header>
         <img class="avatar" src="${avatar}">
@@ -96,48 +51,71 @@ function createTweetElement(tweet) {
       </footer>
     </article>`
   );
-
+  return tweet;
 };
 
+$('')
 
+//========== STAY ON PAGE, SERIALIZE STRING
+$('form[action="/tweets"]').on( "submit", function( event ) {
+  event.preventDefault();
+  var tweetInput = $(this);
+
+  $.ajax({
+    method: 'POST',
+    url: tweetInput.attr('action'),
+    data: tweetInput.find("textarea").serialize()
+  }).done(function () {
+    loadTweets();
+    $('textarea').val('');
+  });
+});
+
+//======= FUNCTION FOR FETCHING TWEETS
+function loadTweets() {
+  $.ajax({
+    method: 'GET',
+    url: '/tweets',
+    dataType: 'json',
+    success: function(tweetData) {
+      renderTweets(tweetData);
+    }
+  });
+}
+loadTweets();
 
 ////////// CORRECT TIME STAMP FOR TWEET
 function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var interval = Math.floor(seconds / 31536000);
-    if (interval > 1) {
-        return interval + " years";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes";
-    }
-    return Math.floor(seconds) + " seconds";
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = Math.floor(seconds / (365 * 24 * 60 * 60));
+  if (interval > 1) {
+    return interval + " years";
+  }
+  interval = Math.floor(seconds / (30 * 24 * 60 * 60));
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(seconds / (24 * 60 * 60));
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(seconds / (60 * 60));
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
 };
 
 
 
 
 
+console.log('DOMContentLoaded has fired');
 
-}
-
-renderTweets(tweetData);
-
-
-  console.log('DOMContentLoaded has fired');
 });
 
 
